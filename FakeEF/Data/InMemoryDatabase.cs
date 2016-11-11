4b0d7a5853acfea8f8c3ef477b4bed4fd6d3c2e6
Christian Giesswein <cgiesswein@live.com>
2016-11-11 15:56:03 +0100

    Fix

--- FakeEF/Data/InMemoryDatabase.cs
+++ FakeEF/Data/InMemoryDatabase.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
 
 using FakeEF.DataHandling;
@@ -20,7 +22,7 @@ namespace FakeEF.Data
 
         private InMemoryDatabase()
         {
-            
+
         }
         internal void SaveChangesInMemory(List<DbEntityEntry> notYetInDatabase)
         {
@@ -124,11 +126,29 @@ namespace FakeEF.Data
                                     .ToList();
                         foreach (var navProperty in navigationProperties)
                         {
-                            var alreadyLoaded = context.ChangeTracker.Entries().FirstOrDefault(x => x.Entity.GetType() == navProperty.PropertyType &&
-                                                                                manager.GetId(x.Entity).Equals(item.Key));
-                            if (alreadyLoaded != null)
+                            if (IsGenericList(navProperty.PropertyType))
                             {
-                                navProperty.SetValue(item.Value, alreadyLoaded.Entity);
+                                var list = navProperty.GetValue(item.Value) as IList;
+                                foreach (var listItem in list.OfType<object>().ToArray())
+                                {
+                                    var alreadyLoaded = LookupIfExistInChangeTracker<T>(context, manager, listItem, listItem.GetType());
+                                    if (alreadyLoaded != null)
+                                    {
+                                        list.Remove(listItem);
+                                        list.Add(alreadyLoaded.Entity);
+                                    }
+                                }
+                                continue;
+                            }
+
+                            var currentValue = navProperty.GetValue(item.Value);
+                            if (currentValue != null)
+                            {
+                                var alreadyLoaded = LookupIfExistInChangeTracker<T>(context, manager, currentValue, navProperty.PropertyType);
+                                if (alreadyLoaded != null)
+                                {
+                                    navProperty.SetValue(item.Value, alreadyLoaded.Entity);
+                                }
                             }
                         }
                     }
@@ -141,6 +161,17 @@ namespace FakeEF.Data
                 }
             }
         }
+
+        private static DbEntityEntry LookupIfExistInChangeTracker<T>(DbContext context, DbSetDataManager manager, object currentValue,
+            Type targetType) where T : class
+        {
+            var associatedId = manager.GetId(currentValue);
+
+            var alreadyLoaded = context.ChangeTracker.Entries().FirstOrDefault(x => x.Entity.GetType() == targetType &&
+                                                                                    manager.GetId(x.Entity).Equals(associatedId));
+            return alreadyLoaded;
+        }
+
         private bool IsGenericList(Type type)
         {
             if (type == null)
@@ -175,7 +206,7 @@ namespace FakeEF.Data
                     PreserveReferencesHandling = PreserveReferencesHandling.Objects,
                     ContractResolver = new FakeEFJsonContractResolver(true, includes)
                 };
-                
+
                 var serialized = JsonConvert.SerializeObject(source, Formatting.None, settings);
                 var result = JsonConvert.DeserializeObject<T>(serialized, settings);
                 return result;
